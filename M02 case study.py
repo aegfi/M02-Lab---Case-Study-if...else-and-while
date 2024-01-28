a2f71871d028337785d8b39320e031308be0e74b
aegfi <130390116+aegfi@users.noreply.github.com>
2024-01-28 18:18:38 -0500

    Update M02 case study.py
    just fixed spelling

--- M02 case study.py
+++ M02 case study.py
@@ -1,17 +1,17 @@
 """
 Author: Amarius Trotter
 name: M02 case study
-desc: test if a student is on Deans list & Honer Roll
+desc: test if a student is on Deans list & Honor roll
 """
-student=str(input("enter students last name: "))
+student=str(input("enter students last name or ZZZ to exit: "))
 while student != "ZZZ":
     Fname=str(input("enter students first name: "))
     gpa=float(input("enter GPA: "))
     if gpa>=3.5:
-        print(Fname+" is on Deans list and Honer Roll")
+        print(Fname+" is on Deans list")
     elif gpa>=3.25:
         print(Fname+" is on Honer Roll")
     else:
-        print("not on Deans list or Honer Roll")
+        print("not on Deans list or Honor Roll")
     print()
-    student=str(input("enter students last name: "))
+    student=str(input("enter students last name or ZZZ to exit: "))
